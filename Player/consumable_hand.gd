142996cb96748596ce52d05cba59b21deae0800a
Elijah Martin <e.troymartin@gmail.com>
2026-06-11 18:21:30 -0400

    Improved animations / integration

--- Player/consumable_hand.gd
+++ Player/consumable_hand.gd
@@ -4,19 +4,31 @@ signal active(value: bool)
 
 @export var consumable_item: item: set = _set_item
 @export var mainhand: Node3D
+@export var offhand: Node3D
 @export var animation_state_machine :AnimationNodeStateMachine
 @export var animation_tree: AnimationTree
 @export var bone_attachment: BoneAttachment3D
-@export var state_chart: StateChart
 @onready var arms_base: Node3D = $"ghoul_arms2(1)"
 @onready var animation_player: AnimationPlayer = $"ghoul_arms2(1)/AnimationPlayer"
-
+var can_activate: bool = true
 func _ready() -> void:
 	#SignalBus.connect("primary_active", _update_primary_active)
 	pass
 
 func _set_item(new_item):
+	if can_activate:
+		can_activate = false
+	else:
+		return
+	var right = mainhand.can_attack
+	var left = offhand.can_activate
+	if left:
+		mainhand.disable()
+	if right:
+		offhand.disable()
+	await get_tree().create_timer(0.25).timeout
 	enable()
+	await get_tree().create_timer(0.25).timeout
 	if new_item != null:
 		consumable_item = new_item
 		if bone_attachment.get_child_count() != 0:
@@ -29,13 +41,18 @@ func _set_item(new_item):
 		await get_tree().create_timer(.1).timeout
 		animation_tree.active = true
 		bone_attachment.add_child(item_add)
-	activate()
-	await animation_player.animation_finished
+		activate()
+	await get_tree().create_timer(0.5).timeout
 	unequip()
 	disable()
+	await get_tree().create_timer(0.25).timeout
+	if right:
+		mainhand.enable()
+	if left:
+		offhand.enable()
+	can_activate = true
 
 func unequip():
-	state_chart.send_event("lower")
 	consumable_item = null
 	for i in bone_attachment.get_children():
 		i.queue_free()
@@ -49,6 +66,5 @@ func disable():
 	tween.tween_property(arms_base, "rotation_degrees:x", -90, .25)
 	
 func enable():
-	visible = true
 	var tween = get_tree().create_tween()
 	tween.tween_property(arms_base, "rotation_degrees:x", 0, .25)
