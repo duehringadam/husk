8a1d3aa7603fb38aa1caf8b2959721268e5663e7
Elijah Martin <e.troymartin@gmail.com>
2026-07-12 14:05:32 -0400

    small improvements & lunge prototype

--- scenes/npc/Corpse Crawler/corpse_crawler.gd
+++ scenes/npc/Corpse Crawler/corpse_crawler.gd
@@ -1,5 +1,11 @@
 extends  npc
 
+@onready var patrol: Node = $StateChart/CompoundState/patrolling/patrol
+
+var timer: float = 0.0
+var search_position : Vector3
+var first_aggro: bool = true
+
 func _physics_process(delta: float) -> void:
 	#var curr_rot = (animation_tree.get_root_motion_rotation_accumulator().inverse() * get_quaternion())
 	#var root_rotation = animation_tree.get_root_motion_rotation()
@@ -9,3 +15,9 @@ func _physics_process(delta: float) -> void:
 	if !is_on_floor():
 		velocity.y -= ProjectSettings.get_setting("physics/3d/default_gravity")
 	move_and_slide()
+
+
+func _on_detection_box_body_entered(body: Node3D) -> void:
+	if body == Global.player:
+		if patrol.is_patrolling:
+			state_chart.send_event("attack")
